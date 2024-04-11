1a1cac2d5bc9be6d8d3a5283d07a157660c3f8bf
Brett Mandler <brettmandler@gmail.com>
2024-04-11 13:54:07 -0400

    Refactor layout.tsx to remove unused imports and update font variables

---
+++ apps/web/config/site.ts
@@ -0,0 +1,12 @@
+export const siteConfig = {
+  name: 'you-got-bud/calendar',
+  url: 'https://calendar.yougotbud.com',
+  description:
+    'A beautiful calendar/date picker. Themed with @shadcn/ui and customizable down to the last element.',
+  links: {
+    twitter: 'https://twitter.com/YouGotBud_',
+    github: 'https://github.com/you-got-bud/calendar',
+  },
+}
+
+export type SiteConfig = typeof siteConfig
