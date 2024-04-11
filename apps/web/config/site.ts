e5b7b235298cc0d344ca981c17f5c42f190a665b
Brett Mandler <brettmandler@gmail.com>
2024-04-11 16:22:30 -0400

    Update site URL in site.ts

--- apps/web/config/site.ts
+++ apps/web/config/site.ts
@@ -1,6 +1,6 @@
 export const siteConfig = {
   name: 'you-got-bud/calendar',
-  url: 'https://calendar-web-psi.vercel.app',
+  url: 'https://calendar.yougotbud.com',
   description:
     'A beautiful calendar/date picker. Themed with @shadcn/ui and customizable down to the last element.',
   links: {
