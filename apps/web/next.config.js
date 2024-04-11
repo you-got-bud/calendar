b5492cc010acf7a0a4cc925f47103400da8d1961
Brett Mandler <brettmandler@gmail.com>
2024-04-11 12:27:19 -0400

    Update calendar component version and styles

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -3,5 +3,6 @@ const {withContentlayer} = require('next-contentlayer')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  transpilePackages: ['@you-got-bud/calendar'],
 }
 module.exports = withContentlayer(nextConfig)
