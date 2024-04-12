cf024a5ea104fb8616372045284bab05524fdeaf
Brett Mandler <brettmandler@gmail.com>
2024-04-12 14:48:26 -0400

    Added sitemap

---
+++ apps/web/app/sitemap.ts
@@ -0,0 +1,20 @@
+import {siteConfig} from '@/config/site'
+import {allDocs} from 'contentlayer/generated'
+import {MetadataRoute} from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: siteConfig.url,
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 1,
+    },
+    ...allDocs.map((doc): MetadataRoute.Sitemap[number] => ({
+      url: `${siteConfig.url}/${doc.slug}`,
+      lastModified: new Date(),
+      changeFrequency: 'hourly',
+      priority: 0.8,
+    })),
+  ]
+}
