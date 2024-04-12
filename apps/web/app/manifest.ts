06c72dcab5ffdcaf75bc4b1088c93db6af63f559
Brett Mandler <brettmandler@gmail.com>
2024-04-12 14:42:31 -0400

    Update manifest.ts to use siteConfig for name, short_name, and description

--- apps/web/app/manifest.ts
+++ apps/web/app/manifest.ts
@@ -1,10 +1,11 @@
+import {siteConfig} from '@/config/site'
 import {MetadataRoute} from 'next'
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: 'Next.js App',
-    short_name: 'Next.js App',
-    description: 'Next.js App',
+    name: siteConfig.name,
+    short_name: siteConfig.name,
+    description: siteConfig.description,
     start_url: '/',
     display: 'standalone',
     background_color: '#fff',
