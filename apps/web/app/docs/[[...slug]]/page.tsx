df6b0296ce75757112b9bfb8d1277e02e33b28f0
Brett Mandler <brettmandler@gmail.com>
2024-04-12 12:34:05 -0400

    Adjust table of contents spacing

--- apps/web/app/docs/[[...slug]]/page.tsx
+++ apps/web/app/docs/[[...slug]]/page.tsx
@@ -95,7 +95,7 @@ export default async function DocPage({params}: DocPageProps) {
       </div>
       {toc && (
         <div className="hidden text-sm xl:block">
-          <div className="sticky top-16 -mt-10 pt-4">
+          <div className="sticky top-24 -mt-10 pt-4">
             <ScrollArea className="pb-10">
               <div className="sticky top-16 -mt-10 h-[calc(100vh-3.5rem)] py-12">
                 <DashboardTableOfContents toc={toc} />
